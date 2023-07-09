fe03870f26dd5fa184d34e5f16fa094229167fb0
vedant2506 <136913764+vedant2506@users.noreply.github.com>
2023-07-09 22:17:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const sr = ScrollReveal({
+    distance: '35px',
+    duration: 2400,
+    reset: true
+});
+
+sr.reveal('.logo',{delay:210,origin:'left'});
+sr.reveal('.navlist',{delay:310,origin:'top'});
+sr.reveal('.top-btnn',{delay:410,origin:'right'});
+
+sr.reveal('.main-content h4',{delay:510,origin:'top'});
+sr.reveal('.main-content h1',{delay:610,origin:'left'});
+sr.reveal('.main-content p',{delay:710,origin:'right'});
+sr.reveal('.social',{delay:810,origin:'left'});
+sr.reveal('.main-btnn',{delay:910,origin:'top'});
+
+sr.reveal('.bottom',{delay:1010,origin:'bottom'});
